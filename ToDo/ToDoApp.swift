62090994b44085f3e58c572c5097e5bbfc62c3c6
shxtrk <direct@shxtrk.dev>
2023-02-26 17:45:18 +0200

    Making todo functionality

--- ToDo/ToDoApp.swift
+++ ToDo/ToDoApp.swift
@@ -6,12 +6,36 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct ToDoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+                store: Store(
+                    initialState: AppState(
+                        todos: [
+                            Todo(
+                                description: "Milk",
+                                id: UUID(),
+                                isComplete: false
+                            ),
+                            Todo(
+                                description: "Eggs",
+                                id: UUID(),
+                                isComplete: false
+                            ),
+                            Todo(
+                                description: "Hand Soap",
+                                id: UUID(),
+                                isComplete: true
+                            ),
+                        ]),
+                    reducer: appReducer,
+                    environment: AppEnvironment()
+                )
+            )
         }
     }
 }
