84ece99a3b17454a1e1e5a3cb80dce10fd933226
shxtrk <direct@shxtrk.dev>
2023-02-26 17:20:33 +0200

    Project initialisation

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Serhii Striuk on 26.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
