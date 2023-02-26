95fe51e280a298c385a4a2033f4be897f6e27c77
shxtrk <direct@shxtrk.dev>
2023-02-26 18:40:26 +0200

    Collections of domain

--- ToDo/ContentView.swift
+++ ToDo/ContentView.swift
@@ -14,27 +14,51 @@ struct Todo: Equatable, Identifiable {
     var isComplete = false
 }
 
+enum TodoAction: Equatable {
+    case checkboxTapped
+    case textFieldChanged(String)
+}
+
+struct TodoEnvironment { }
+
+let todoReducer = AnyReducer<Todo, TodoAction, TodoEnvironment> { state, action, environment in
+    switch action {
+    case .checkboxTapped:
+        state.isComplete.toggle()
+        return .none
+    case .textFieldChanged(let text):
+        state.description = text
+        return .none
+    }
+}
+
 struct AppState: Equatable {
     var todos: [Todo] = []
 }
 
-enum AppAction {
-    case todoCheckboxTapped(index: Int)
-    case todoTextFieldChanged(index: Int, text: String)
+enum AppAction: Equatable {
+    case addButtonTapped
+    case todo(index: Int, action: TodoAction)
 }
 
 struct AppEnvironment { }
 
-let appReducer = AnyReducer<AppState, AppAction, AppEnvironment> { state, action, environment in
-    switch action {
-    case .todoCheckboxTapped(index: let index):
-        state.todos[index].isComplete.toggle()
-        return .none
-    case .todoTextFieldChanged(index: let index, text: let text):
-        state.todos[index].description = text
-        return .none
+let appReducer = AnyReducer<AppState, AppAction, AppEnvironment>.combine(
+    todoReducer.forEach(
+        state: \AppState.todos,
+        action: /AppAction.todo(index:action:),
+        environment: { _ in TodoEnvironment() }
+    ),
+    AnyReducer { state, action, environment in
+        switch action {
+        case .addButtonTapped:
+            state.todos.insert(Todo(id: UUID()), at: 0)
+            return .none
+        case .todo(index: let index, action: let action):
+            return .none
+        }
     }
-}
+)
     .debug()
 
 struct ContentView: View {
@@ -44,26 +68,40 @@ struct ContentView: View {
         NavigationView {
             WithViewStore(self.store) { viewStore in
                 List {
-                    ForEach(Array(viewStore.todos.enumerated()), id: \.element.id) { index, todo in
-                        HStack {
-                            Button(action: { viewStore.send(.todoCheckboxTapped(index: index)) }) {
-                                Image(systemName: todo.isComplete ? "checkmark.square" : "square")
-                            }
-                            .buttonStyle(PlainButtonStyle())
-                            
-                            TextField(
-                                "Untitled todo",
-                                text: viewStore.binding(
-                                    get: { $0.todos[index].description },
-                                    send: { .todoTextFieldChanged(index: index, text: $0) }
-                                )
-                            )
-                        }
-                        .foregroundColor(todo.isComplete ? .gray : nil)
-                    }
+                    ForEachStore(
+                        self.store.scope(state: \.todos, action: AppAction.todo(index:action:)),
+                        content: TodoView.init(store:)
+                    )
                 }
                 .navigationBarTitle("Todos")
+                .navigationBarItems(trailing: Button("Add") {
+                    viewStore.send(.addButtonTapped)
+                })
             }
+        }
+    }
+}
+
+struct TodoView: View {
+    let store: Store<Todo, TodoAction>
+    
+    var body: some View {
+        WithViewStore(self.store) { viewStore in
+            HStack {
+                Button(action: { viewStore.send(.checkboxTapped) }) {
+                    Image(systemName: viewStore.isComplete ? "checkmark.square" : "square")
+                }
+                .buttonStyle(PlainButtonStyle())
+                
+                TextField(
+                    "Untitled todo",
+                    text: viewStore.binding(
+                        get: \.description,
+                        send: TodoAction.textFieldChanged
+                    )
+                )
+            }
+            .foregroundColor(viewStore.isComplete ? .gray : nil)
         }
     }
 }
